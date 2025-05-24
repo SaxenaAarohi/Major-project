b438011a0ce0930ab0970257ef81291adc18cf5e
AarohiSaxena10 <aarohisaxena1014@gmail.com>
2025-05-24 10:58:12 +0530

    edit

--- src/Component/Addcolumn.jsx
+++ src/Component/Addcolumn.jsx
@@ -15,7 +15,7 @@ const Addcolumn = () => {
 
     return (
         <div>
-            <div onClick={handlemodal} className='flex justify-center items-center hover:bg-slate-600 w-20 ml-4 md:w-52 bg-[linear-gradient(to_bottom,_#d4dce3,_#eef2f3)] h-[80%] md:h-[calc(108vh-80px)]'>
+            <div onClick={handlemodal} className='flex justify-center items-center hover:bg-slate-600 w-20 ml-4 md:w-52 bg-[linear-gradient(to_bottom,_#d4dce3,_#eef2f3)]  h-[calc(108vh-80px)]'>
                + New column
             </div>
             {isopen && <Modal onclose={handleclose}/>}
